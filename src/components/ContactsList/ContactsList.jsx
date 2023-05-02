fa1330e8f465812adda1639b954e24ec91ac8bf6
Korotetska Anna <avkorotetska@gmail.com>
2023-05-02 16:22:12 +0300

    add files

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ContactsListEl, ListItem, Button } from './ContactsListStyled';
+
+export class ContactsList extends Component {
+  render() {
+    const { contacts, onClick } = this.props;
+
+    return (
+      <ContactsListEl>
+        {contacts.map(({ name, id, number }) => (
+          <ListItem key={id}>
+            {name}: {number}
+            <Button onClick={() => onClick(id)}>Delete</Button>
+          </ListItem>
+        ))}
+      </ContactsListEl>
+    );
+  }
+}
+
+ContactsList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+  onClick: PropTypes.func.isRequired,
+};
