7865db092def1401011ca4e67ff9c366f17b62f8
Korotetska Anna <avkorotetska@gmail.com>
2023-05-03 08:40:06 +0300

    fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -16,6 +16,6 @@ export class Filter extends Component {
 }
 
 Filter.propTypes = {
-  onChange: PropTypes.func,
-  value: PropTypes.string
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
 };
